21afadf40b050d4950940969146b690dc2e1b833
Titus Mumo <titusmumo@gmail.com>
2022-08-21 23:00:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+
+//First test
+function findMatching (driver, attribute) {
+    return driver.filter(function(drivers){
+        return drivers.toLowerCase() === attribute.toLowerCase()
+    })
+}
+
+//Second test
+function fuzzyMatch(driver, attribute) {
+    return driver.filter(function(drivers){
+        return drivers[0] === attribute[0]
+    })
+}
+
+//Third test
+function matchName(driver, attribute) {
+    return driver.filter(function(drivers){
+        return drivers.name === attribute
+    })
+}
